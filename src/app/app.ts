219f0d61d610277b760874163967e5b3da22569d
Jagova <pacojagova@gmail.com>
2025-09-01 15:46:29 -0600

    2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilmsList } from "./components/films-list/films-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FilmsList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
