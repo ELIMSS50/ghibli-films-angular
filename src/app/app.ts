39cfc35d1e663fdac4f538a022388f1f627939ac
Jagova <pacojagova@gmail.com>
2025-09-09 17:13:27 -0600

    Angular routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FilmsList } from "./components/films-list/films-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FilmsList],
+  imports: [RouterOutlet, FilmsList, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/create-film-page/create-film-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-film-page',
+  imports: [],
+  templateUrl: './create-film-page.html',
+  styleUrl: './create-film-page.css'
+})
+export class CreateFilmPage {
+
+}
